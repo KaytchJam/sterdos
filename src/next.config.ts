4c3aa760c2f71d224f748547df18762362ab3b01
KJam <kaytchjam@gmail.com>
2026-02-17 00:56:16 -0600

    config file

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const path = require("path");
+
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.join(__dirname, '.'),
+  },
+  output: "standalone"
 };
 
 export default nextConfig;
