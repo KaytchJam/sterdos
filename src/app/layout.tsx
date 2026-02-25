d61ba59ed8a9842e3a380ad7ac201892eefbca1e
KJam <kaytchjam@gmail.com>
2026-02-24 22:14:13 -0600

    added metadata and viewport info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,16 @@
 import './globals.css';
+import type { Metadata, Viewport } from 'next'
+
+export const metadata: Metadata = {
+  title: "Sterdos",
+  description: "Mobility score calculator",
+  keywords: "Sterdos, Bus, METRO, Houston, Transit, 15 minute, 15 minute bus, bus nearby, bus close to me, bus app, transit score"
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1
+}
 
 export default function RootLayout({ children, }: {children: React.ReactNode}) {
     return (
